12b8f29dd28d00812d956400922993a23066b2ae
Cesar Acual <chechaacual@gmail.com>
2018-11-21 23:46:29 -0600

    PHPMySQL-Connect

--- public/index.php
+++ public/index.php
@@ -1,18 +1,10 @@
 <?php
 
-if (!isset($_POST['firstname'])) {
-    include __DIR__ . '/../templates/form.html.php';
-} else {
-    $firstName = $_POST['firstname'];
-    $lastName = $_POST['lastname'];
-
-    if ($firstName == 'Kevin' && $lastName == 'Yank') {
-        $output = 'Welcome, oh glorius leader!';
-    } else {
-        $output = 'Welcome to our website, ' .
-            htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
-            htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
-    }
-
-    include __DIR__ . '/../templates/welcome.html.php';
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=ijdb', 'ijdb', 'ijdb');
+    $output = 'Database connection established.';
+} catch (PDOException $e) {
+    $output = 'Unable to connect to the database server: ' . $e;
 }
+
+include __DIR__ . '/../templates/output.html.php';
--- templates/output.html.php
+++ templates/output.html.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Form Example</title>
+		<title>Script Output</title>
 	</head>
 	<body>
 		<p>
