c412693ec066f197b17a0abddbb609e9046e8613
Cesar Acual <chechaacual@gmail.com>
2018-11-23 18:50:30 -0600

    MySQL-Create

--- public/index.php
+++ public/index.php
@@ -3,9 +3,23 @@
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdb', 'ijdb');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $output = 'Database connection established.';
+
+    $sql = 'CREATE TABLE joke (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        joketext TEXT,
+        jokedate DATE NOT NULL
+    ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+
+    $pdo->exec($sql);
+
+    $output = 'Joke table successfully created.';
 } catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e;
+    $output = sprintf(
+        'Database error: %s in %s:%s',
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine()
+    );
 }
 
 include __DIR__ . '/../templates/output.html.php';
