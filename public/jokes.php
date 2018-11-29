1826a0f85eef8c0b3bdda911028e7667d4d6931b
Cesar Acual <chechaacual@gmail.com>
2018-11-28 22:52:08 -0600

    MySQL-ListJokes-Layout-3

--- public/jokes.php
+++ public/jokes.php
@@ -13,15 +13,21 @@ try {
 
     $title = 'Joke list';
 
-    $output = '';
+    // Start the buffer
 
-    foreach ($jokes as $joke ) {
-        $output .= '<blockquote>';
-        $output .= '<p>';
-        $output .= $joke;
-        $output .= '</p>';
-        $output .= '</blockquote>';
-    }
+    ob_start();
+
+    // Include the template, The PHP code will be executed,
+    // but the resulting HTML will be stored in the buffer
+    // rather than sent to the browser
+
+    include __DIR__ . '/../templates/jokes.html.php';
+
+    // Read the contents fo the output buffer and store them
+    // in the $output variable for use in layout.html.php
+
+    $output = ob_get_clean();
+    
 } catch (PDOException $e) {
     $title = 'An error has ocurred';
 
