6a8f17dd3c1107ee5afa56f3cbd6f1f7b191ba2e
Cesar Acual <chechaacual@gmail.com>
2018-12-05 23:10:44 -0600

    Remove extra verification

--- public/deletejoke.php
+++ public/deletejoke.php
@@ -1,26 +1,24 @@
 <?php
 
-if (isset($_POST['id'])) {
-    try {
-        $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdb', 'ijdb');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdb', 'ijdb');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = 'DELETE FROM `joke` WHERE `id` = :id';
+    $sql = 'DELETE FROM `joke` WHERE `id` = :id';
 
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':id', $_POST['id']);
-        $stmt->execute();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $_POST['id']);
+    $stmt->execute();
 
-        header('location: jokes.php');
-    } catch (PDOException $e) {
-        $title = 'An error has ocurred';
+    header('location: jokes.php');
+} catch (PDOException $e) {
+    $title = 'An error has ocurred';
 
-        $output = sprintf("Unable to connect to the database server: %s in %s:%s",
-            $e->getMessage(),
-            $e->getFile(),
-            $e->getLine()
-        );
-    }
-    
-    include __DIR__ . '/../templates/layout.html.php';
+    $output = sprintf("Unable to connect to the database server: %s in %s:%s",
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine()
+    );
 }
+
+include __DIR__ . '/../templates/layout.html.php';
