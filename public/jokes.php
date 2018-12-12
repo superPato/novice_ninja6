d0f801b790d5e0250f63d20b8352a098c69e35bd
Cesar Acual <chechaacual@gmail.com>
2018-12-12 11:32:00 -0600

    DBDesign-JokeAuthor

--- public/jokes.php
+++ public/jokes.php
@@ -4,7 +4,9 @@ try {
     $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8', 'ijdb', 'ijdb');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT `id`, `joketext` FROM `joke`';
+    $sql = 'SELECT `joke`.`id`, `joketext`, `name`, `email` 
+        FROM `joke` INNER JOIN `author`
+            ON `authorid` = `author`.`id`';
     
     $jokes = $pdo->query($sql);
 
--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -2,6 +2,11 @@
 <blockquote>
     <p>
         <?= htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8') ?>
+
+        (by <a href="mailto:<?php echo htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8'); ?>">
+            <?php echo htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8'); ?>
+        </a>)
+        
         <form action="deletejoke.php" method="POST">
             <input type="hidden" name="id" value="<?= $joke['id'] ?>">
             <input type="submit" value="Delete">
