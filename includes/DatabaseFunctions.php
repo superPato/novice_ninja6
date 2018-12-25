cb0a6ee4159d3e0f784127d49f838ace7c0973c9
Cesar Acual <chechaacual@gmail.com>
2018-12-24 22:55:51 -0600

    Structure-TotalJokes

---
+++ includes/DatabaseFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+function totalJokes($database)
+{
+    $query = $database->prepare('SELECT COUNT(*) FROM `joke`');
+    $query->execute();
+
+    $row = $query->fetch();
+
+    return $row[0];
+}
--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,3 +1,5 @@
+<p><?= $totalJokes ?> jokes have been submitted to the Internet Joke Database.</p>
+
 <?php foreach ($jokes as $joke): ?>
 <blockquote>
     <p>
@@ -6,7 +8,7 @@
         (by <a href="mailto:<?php echo htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8'); ?>">
             <?php echo htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8'); ?>
         </a>)
-        
+
         <form action="deletejoke.php" method="POST">
             <input type="hidden" name="id" value="<?= $joke['id'] ?>">
             <input type="submit" value="Delete">
