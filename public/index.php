474248ecff467348c9ee71252e68a43a6b22146d
Cesar Acual <chechaacual@gmail.com>
2019-03-13 18:56:12 -0600

    CMS-Controller

--- public/index.php
+++ public/index.php
@@ -14,20 +14,38 @@ function loadTemplate($templateFileName, $variables = [])
 try {
     include __DIR__ . '/../includes/DatabaseConnection.php';
     include __DIR__ . '/../classes/DatabaseTable.php';
-    include __DIR__ . '/../classes/controllers/JokeController.php';
 
     $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
     $authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
-    $jokeController = new JokeController($jokesTable, $authorsTable);
+    // if no route variables is set, use 'joke/home'
+    $route = $_GET['route'] ?? 'joke/home';
 
-    $action = $_GET['action'] ?? 'home';
-
-    if ($action == strtolower($action)) {
-        $page = $jokeController->$action();
+    if ($route == strtolower($route)) {
+        if ($route == 'joke/list') {
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->list();
+        } elseif ($route == 'joke/home') {
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->home();
+        } elseif ($route == 'joke/edit') {
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->edit();
+        } elseif ($route == 'joke/delete') {
+            include __DIR__ . '/../classes/controllers/JokeController.php';
+            $controller = new JokeController($jokesTable, $authorsTable);
+            $page = $controller->delete();
+        } elseif ($route == 'register') {
+            include __DIR__ . '/../classes/controllers/RegisterController.php';
+            $controller = new RegisterController($authorsTable);
+            $page = $controller->showForm();
+        }
     } else {
         http_response_code(301);
-        header('location: index.php?action=' . strtolower($action));
+        header('location: index.php?route=' . strtolower($route));
     }
 
     $title  = $page['title'];
