b3b16718680dacc9e9ad83d05f9b741e8819a57a
Cesar Acual <chechaacual@gmail.com>
2019-05-06 17:31:28 -0600

    Registration Validation

--- classes/Ijdb/Controllers/Register.php
+++ classes/Ijdb/Controllers/Register.php
@@ -17,9 +17,41 @@ class Register
 	{
 		$author = $_POST['author'];
 
-		$this->authorsTable->save($author);
+		// Assume the data is valid to begin with
+		$valid = true;
+		$errors = [];
 
-		header('Location: /author/success');
+		// But if any of the fields have been left blank
+		// set $valid to false
+		if (empty($author['name'])) {
+			$valid = false;
+			$errors[] = 'Name cannot be blank';
+		}
+		if (empty($author['email'])) {
+			$errors[] = 'Email cannot be blank';
+			$valid = false;
+		}
+		if (empty($author['password'])) {
+			$valid = false;
+			$errors[] = 'Password cannot be blank';
+		}
+
+		// If $valid is still true, no fields were blank
+		// and the data can be added
+		if ($valid == true) {
+			$this->authorsTable->save($author);
+
+			header('Location: /author/success');
+		} else {
+			return [
+				'template'  => 'register.html.php',
+				'title'     => 'Register an account',
+				'variables' => [
+					'errors' => $errors,
+					'author' => $author
+				]
+			];
+		}
 	}
 
 	public function registrationForm()
