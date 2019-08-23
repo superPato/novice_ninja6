523621273e36f8ae89212f990ad73573cfc09f0f
Cesar Acual <chechaacual@gmail.com>
2019-08-23 11:58:07 -0600

    Relationships EditJoke

--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -1,11 +1,11 @@
-<?php if (empty($joke['id']) || $userid == $joke['authorid']): ?>
+<?php if (empty($joke->id) || $userid == $joke->authorid): ?>
 	
 <form action="" method="post">
-	<input type="hidden" name="joke[id]" value="<?= $joke['id'] ?? '' ?>">
+	<input type="hidden" name="joke[id]" value="<?= $joke->id ?? '' ?>">
 
 	<label for="joketext">Type your joke here:</label>
 	<textarea name="joke[joketext]" id="joketext" cols="40" rows="3">
-		<?= $joke['joketext'] ?? '' ?>
+		<?= $joke->joketext ?? '' ?>
 	</textarea>
 	<input type="submit" name="submit" value="Save">
 </form>
