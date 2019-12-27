60dfbded31b4b481df37f3ab3e1358ba9d3673ba
Cesar Acual <chechaacual@gmail.com>
2019-12-27 11:39:51 -0600

    Formating joke text

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -12,34 +12,31 @@
 
         <?php foreach ($jokes as $joke): ?>
         <blockquote>
-            <p>
-                <?= htmlspecialchars($joke->joketext, ENT_QUOTES, 'UTF-8') ?>
+            <?= (new \Ninja\Markdown($joke->joketext))->toHtml() ?>
 
-                (by <a href="mailto:<?php echo htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8'); ?>">
-                    <?php echo htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8'); ?>
-                </a> on
-                <?php $date = new DateTime($joke->jokedate);
-                    echo $date->format('jS F Y');
-                ?>)
+            (by <a href="mailto:<?php echo htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8'); ?>">
+                <?php echo htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8'); ?>
+            </a> on
+            <?php $date = new DateTime($joke->jokedate);
+                echo $date->format('jS F Y');
+            ?>)
 
-                <?php if ($user): ?>
+            <?php if ($user): ?>
 
                 <?php if ($user->id == $joke->authorid || 
-                          $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKES)): ?>
+                            $user->hasPermission(\Ijdb\Entity\Author::EDIT_JOKES)): ?>
                 <a href="/joke/edit?id=<?= $joke->id ?>">Edit</a>
                 <?php endif; ?>
 
                 <?php if ($user->id == $joke->authorid || 
-                          $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKES)): ?>
+                            $user->hasPermission(\Ijdb\Entity\Author::DELETE_JOKES)): ?>
                 <form action="/joke/delete" method="POST">
                     <input type="hidden" name="id" value="<?= $joke->id ?>">
                     <input type="submit" value="Delete">
                 </form>
                 <?php endif ?>
 
-                <?php endif; ?>
-
-            </p>
+            <?php endif; ?>
         </blockquote>
         <?php endforeach; ?>
     </div>
